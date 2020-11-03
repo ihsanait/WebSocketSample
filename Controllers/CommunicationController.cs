7f34ee9aac9d921dd4d5c1f2cc413cbdb2103ad9
ihsanait <ihsan@ait.com.tr>
2020-11-03 18:12:45 +0300

    Init project

---
+++ Controllers/CommunicationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketSample.Controllers
+{
+    [ApiController, Route("Comm")]
+    public class CommunicationController : ControllerBase
+    {
+        public CommunicationController()
+        {
+            
+        }
+
+
+    }
+}
